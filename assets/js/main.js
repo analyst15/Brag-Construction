d1d2ca91147febf191db390a1b22361fb03fdd30
Alex Okeyo <alexokkeyo15@gmail.com>
2022-07-05 13:58:16 +0300

    New Changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -148,4 +148,30 @@ document.addEventListener('DOMContentLoaded', () => {
     aos_init();
   });
 
+  new Swiper('#property-carousel', {
+    speed: 600,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.propery-carousel-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 20
+      },
+
+      1200: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      }
+    }
+  });
+
 });
